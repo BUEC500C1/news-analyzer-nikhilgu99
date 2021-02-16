49dcab0116f308f45ce9be4394f67ee80a69b244
Nikhil Gupta <99nikhilgupta@gmail.com>
2021-02-16 18:32:59 -0500

    Updated upload.py with stub functionality

--- upload.py
+++ upload.py
@@ -1,1 +1,16 @@
 # Secure File Uploader / Ingestor
+
+# Data
+
+# file -> the file to be converted
+status = ""
+
+#Functions
+
+def upload(filename):
+
+    return "File Upload Successful!"
+
+def checkStatus():
+
+    return "Failure - File Too Large!"
