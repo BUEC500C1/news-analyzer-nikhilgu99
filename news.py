4964d43c3185543e1304d28e254be17295d36238
Nikhil Gupta <99nikhilgupta@gmail.com>
2021-02-20 21:56:57 -0500

    Update news.py

--- news.py
+++ news.py
@@ -3,3 +3,20 @@
 # Data
 
 # Functions
+
+def addURL(url, filename):
+
+    return("Successfully added URL")
+
+def findKeywords(number):
+
+    return ["word1", "word2"]
+
+def findNews(number, words, filename):
+
+    return("Successfully found and added news")
+
+def findSentiment(filename):
+
+    return("Determined Sentiment: Positive")
+
