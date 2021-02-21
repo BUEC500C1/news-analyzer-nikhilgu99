e3d7a09a221149b42f20e495f4ccb797121df41c
Nikhil Gupta <99nikhilgupta@gmail.com>
2021-02-20 19:47:20 -0500

    Created NLP.py

---
+++ NLP.py
@@ -0,0 +1,31 @@
+# Text NLP Analysis
+
+# Data
+
+class fullNLP:
+    def __init__(self, sentiment, links):
+        self.sentiment = sentiment # String with NLP determined sentiment
+        self.relevantLinks = links # List of determined relevant links, default to 5?
+
+
+# Functions
+
+def upload(filename):
+
+    return("File successfully uploaded.")
+
+def getFullAnalysis():
+
+    return 0
+
+def getTranslation(language):
+
+    return("Translation successful")
+
+def getSentiment():
+
+    return("Positive")
+
+def getRelevantLinks(number):
+
+    return("Successfully found and saved links")
